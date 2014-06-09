598f4c58d509c9503063be9d1d11068bb6e3d671
Aggelos Orfanakos <me@agorf.gr>
2014-06-09 13:52:59 +0300

    Spec Feed2Email::VERSION

---
+++ spec/lib/version_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Feed2Email do
+  it 'has a version constant' do
+    expect { Feed2Email::VERSION }.not_to raise_error
+    expect(Feed2Email::VERSION).to match /\A\d+\.\d+\.\d+\z/
+  end
+end
