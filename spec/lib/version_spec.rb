aeaf16ac57c25182536fe9762473e20ce42bcf7e
Aggelos Orfanakos <me@agorf.gr>
2014-07-09 15:04:45 +0300

    Capitalize version constant in spec

--- spec/lib/version_spec.rb
+++ spec/lib/version_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Feed2Email do
-  it 'has a version constant' do
+  it 'has a VERSION constant' do
     expect { Feed2Email::VERSION }.not_to raise_error
     expect(Feed2Email::VERSION).to match /\A\d+\.\d+\.\d+\z/
   end
