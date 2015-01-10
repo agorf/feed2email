f4d8b92dc239f325d3554377258faeb5122e9c52
Aggelos Orfanakos <me@agorf.gr>
2015-01-10 18:22:27 +0100

    Use instance variables in module scope

--- lib/feed2email.rb
+++ lib/feed2email.rb
@@ -1,19 +1,19 @@
+require 'feed2email/config'
+require 'feed2email/logger'
+
 module Feed2Email
   CONFIG_DIR = File.expand_path('~/.feed2email')
 
-  def self.config
-    @config ||= Config.new(File.join(CONFIG_DIR, 'config.yml'))
-  end
+  def self.config; @config end
 
-  def self.logger
-    @logger ||= Logger.new(config['log_path'], config['log_level'],
-                           config['log_shift_age'], config['log_shift_size'])
-  end
+  def self.logger; @logger end
 
   def self.log(*args)
     logger.log(*args) # delegate
   end
-end
 
-require 'feed2email/config'
-require 'feed2email/logger'
+  @config = Config.new(File.join(CONFIG_DIR, 'config.yml'))
+
+  @logger = Logger.new(config['log_path'], config['log_level'],
+                       config['log_shift_age'], config['log_shift_size'])
+end
