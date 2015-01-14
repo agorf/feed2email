51f5de99a47216546efc81e3334cb64f97729dd4
Aggelos Orfanakos <me@agorf.gr>
2015-01-14 12:37:40 +0100

    Move @config, @logger, @smtp_connection back to class methods
    Otherwise, assignment (and thus, their initialization) takes place
    during parse time, even though they may never be used.

--- lib/feed2email.rb
+++ lib/feed2email.rb
@@ -5,18 +5,18 @@ require 'feed2email/logger'
 module Feed2Email
   CONFIG_DIR = File.expand_path('~/.feed2email')
 
-  def self.config; @config end
-
-  def self.logger
-    @logger.logger # delegate
+  def self.config
+    @config ||= Config.new(File.join(CONFIG_DIR, 'config.yml'))
   end
 
-  def self.smtp_connection; @smtp_connection end
+  def self.logger
+    @logger ||= Logger.new(
+      config['log_path'], config['log_level'], config['log_shift_age'],
+      config['log_shift_size']
+    ).logger
+  end
 
-  @config = Config.new(File.join(CONFIG_DIR, 'config.yml'))
-
-  @logger = Logger.new(config['log_path'], config['log_level'],
-                       config['log_shift_age'], config['log_shift_size'])
-
-  @smtp_connection = LazySMTPConnection.new
+  def self.smtp_connection
+    @smtp_connection ||= LazySMTPConnection.new
+  end
 end
