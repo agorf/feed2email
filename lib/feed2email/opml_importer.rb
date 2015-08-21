f34287d14649e93a1403135f858ce5cb1df556e1
Angelos Orfanakos <me@agorf.gr>
2015-08-21 11:53:28 +0300

    Simplify OPMLImporter.import
    By using `OPMLImporter#feeds` to get a list of feed URIs instead of
    passing a block to `OPMLImporter#import`.

--- lib/feed2email/opml_importer.rb
+++ lib/feed2email/opml_importer.rb
@@ -5,48 +5,46 @@ module Feed2Email
     def self.import(path, remove = false)
       require 'feed2email/feed'
 
-      n = 0
+      feeds = open(path) {|f| new(f).feeds }
 
-      open(path) do |f|
-        uris = []
+      imported = 0
 
-        new(f).import do |uri|
-          if feed = Feed[uri: uri]
-            warn "Feed already exists: #{feed}"
+      feeds.each do |uri|
+        if feed = Feed[uri: uri]
+          warn "Feed already exists: #{feed}"
+        else
+          feed = Feed.new(uri: uri)
+
+          if feed.save(raise_on_failure: false)
+            puts "Imported feed: #{feed}"
+            imported += 1
           else
-            feed = Feed.new(uri: uri)
-
-            if feed.save(raise_on_failure: false)
-              puts "Imported feed: #{feed}"
-              n += 1
-            else
-              warn "Failed to import feed: #{feed}"
-            end
-          end
-
-          uris << uri
-        end
-
-        if remove
-          Feed.exclude(uri: uris).each do |feed|
-            if feed.delete
-              puts "Removed feed: #{feed}"
-            else
-              warn "Failed to remove feed: #{feed}"
-            end
+            warn "Failed to import feed: #{feed}"
           end
         end
       end
 
-      n
+      if remove
+        Feed.exclude(uri: uris).each do |feed|
+          if feed.delete
+            puts "Removed feed: #{feed}"
+          else
+            warn "Failed to remove feed: #{feed}"
+          end
+        end
+      end
+
+      imported
     end
 
     def initialize(io)
       @io = io
     end
 
-    def import(&blk)
-      uris.each(&blk)
+    def feeds
+      Nokogiri::XML(data).css('opml body outline').map {|outline|
+        outline['xmlUrl']
+      }.compact
     end
 
     private
@@ -56,11 +54,5 @@ module Feed2Email
     end
 
     def io; @io end
-
-    def uris
-      Nokogiri::XML(data).css('opml body outline').map {|outline|
-        outline['xmlUrl']
-      }.compact
-    end
   end
 end
