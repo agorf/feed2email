agorf/feed2email

commit 82208f79c93faeba585b51a2fe326e2292635630

Use instance variables to restore last_modified/etag
Thus removing the dependency of the "dirty" Sequel plugin.

lib/feed2email/feed.rb
@@ -14,7 +14,6 @@ require 'feed2email/version'
 
 module Feed2Email
   class Feed < Sequel::Model(:feeds)
-    plugin :dirty
     plugin :timestamps
 
     one_to_many :entries
@@ -38,11 +37,13 @@ module Feed2Email
         return
       end
 
+      old_last_modified, old_etag = last_modified, etag
+
       # Reset feed caching parameters unless all entries were processed. This
       # makes sure the feed will be fetched on next processing.
       unless process_entries
-        self.last_modified = initial_value(:last_modified)
-        self.etag = initial_value(:etag)
+        self.last_modified = old_last_modified
+        self.etag = old_etag
       end
 
       self.last_processed_at = Time.now

commit 0f3dac5305127976e2fc38ee71b344d01cad8fc0

Store old last_modified/etag before feed fetching
Where it's modified. Broken with 82208f7.

lib/feed2email/feed.rb
@@ -30,14 +30,14 @@ module Feed2Email
     def process
       logger.info "Processing feed #{uri} ..."
 
+      old_last_modified, old_etag = last_modified, etag
+
       return false unless fetch_and_parse
 
       unless processable?
         logger.warn 'Feed does not have entries'
         return
       end
-
-      old_last_modified, old_etag = last_modified, etag
 
       # Reset feed caching parameters unless all entries were processed. This
       # makes sure the feed will be fetched on next processing.