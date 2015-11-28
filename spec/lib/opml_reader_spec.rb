257dc1caab0a25d43195a03d5e96369be2be58f5
Angelos Orfanakos <me@agorf.gr>
2015-11-28 17:36:10 +0200

    Spec Feed2Email::OPMLReader

---
+++ spec/lib/opml_reader_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+require 'feed2email/opml_reader'
+
+describe Feed2Email::OPMLReader do
+  let(:io) { File.open(File.join(%w{spec fixtures feeds.opml})) }
+
+  subject(:opml_reader) { Feed2Email::OPMLReader.new(io) }
+
+  describe '#feeds' do
+    subject { opml_reader.feeds }
+
+    it 'returns a list of feed URIs' do
+      expect(subject).to eq [
+        'https://github.com/agorf.atom',
+        'https://github.com/agorf/feed2email/commits/master.atom'
+      ]
+    end
+  end
+end
