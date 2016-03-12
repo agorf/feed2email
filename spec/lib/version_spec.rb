55bb971f8fbe1b994f78e69f37e59c11bc2328d4
Angelos Orfanakos <me@agorf.gr>
2016-03-12 10:04:44 +0200

    Wrap regexp with parentheses to silence syntastic warning

--- spec/lib/version_spec.rb
+++ spec/lib/version_spec.rb
@@ -5,6 +5,6 @@ describe Feed2Email do
   subject { Feed2Email::VERSION }
 
   it 'has a VERSION constant with a major.minor.patch format' do
-    expect(subject).to match /\A\d+\.\d+\.\d+\z/
+    expect(subject).to match(/\A\d+\.\d+\.\d+\z/)
   end
 end
