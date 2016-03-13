agorf/feed2email

commit 097a193dffe27a64782281b25e62187928658a3c

Use attr_accessor for parsed_feed

lib/feed2email/feed.rb
@@ -106,8 +106,8 @@ module Feed2Email
 
     def fetch_and_parse
       if xml_data = fetch
-        @parsed_feed = parse(xml_data)
-        @parsed_feed && @parsed_feed.respond_to?(:entries)
+        self.parsed_feed = parse(xml_data)
+        parsed_feed && parsed_feed.respond_to?(:entries)
       end
     end
 
@@ -163,7 +163,7 @@ module Feed2Email
       parsed_feed.entries
     end
 
-    attr_reader :parsed_feed
+    attr_accessor :parsed_feed
 
     def process_entries
       total = processable_entries.size

commit ea2f1b7d929b26ea0b01162fd21fcc2cd2fd7650

Set parsed_feed with instance variable
It's not possible to set it with an accessor (writer) and have it be
private at the same time.

Broken by 097a193dffe27a64782281b25e62187928658a3c

lib/feed2email/feed.rb
@@ -109,7 +109,7 @@ module Feed2Email
 
     def fetch_and_parse
       if xml_data = fetch
-        self.parsed_feed = parse(xml_data)
+        @parsed_feed = parse(xml_data)
         parsed_feed && parsed_feed.respond_to?(:entries)
       end
     end
@@ -159,7 +159,7 @@ module Feed2Email
       parsed_feed.entries
     end
 
-    attr_accessor :parsed_feed
+    attr_reader :parsed_feed
 
     def process_entries
       total = processable_entries.size