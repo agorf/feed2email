agorf/feed2email

commit 070916be4486658d5199d30ae5fb75fb7d857ddd

Allow implicit returns

lib/feed2email/feed.rb
@@ -85,20 +85,17 @@ module Feed2Email
         Feed2Email::HTTPFetcher.new(uri, request_headers: fetch_headers) do |f|
           if f.response.is_a?(Net::HTTPNotModified)
             logger.info 'Feed not modified; skipping...'
-
             return false
           end
 
           self.last_modified = f.response['last-modified']
           self.etag = f.response['etag']
-
-          return f.data
+          f.data
         end
       rescue => e
         logger.error 'Failed to fetch feed'
         record_exception(e)
-
-        return false
+        false
       end
     end
 
@@ -153,7 +150,7 @@ module Feed2Email
       rescue => e
         logger.error 'Failed to parse feed'
         record_exception(e)
-        return false
+        false
       end
     end
 
@@ -183,10 +180,10 @@ module Feed2Email
 
       begin
         logger.info "Processing entry #{index}/#{total} #{entry_url} ..."
-        return entry.process
+        entry.process
       rescue => e
         record_exception(e)
-        return false
+        false
       end
     end
 

lib/feed2email/entry.rb
@@ -40,7 +40,7 @@ module Feed2Email
         return true
       end
 
-      return send_mail
+      send_mail
     end
 
     private
@@ -122,7 +122,7 @@ module Feed2Email
       if build_mail.deliver!
         self.last_email_sent_at = Time.now
         save # record as seen
-        return true
+        true
       end
     end
 

lib/feed2email/feed_analyzer.rb
@@ -35,9 +35,9 @@ module Feed2Email
 
       case File.basename(path)
       when 'rss.xml', 'rdf.xml'
-        return 'rss'
+        'rss'
       when 'atom.xml'
-        return 'atom'
+        'atom'
       end
     end
 

commit 6cd4706642605d3c927fc32dfb7b7cdc3c61ff02

Fix #fetch to return data instead of HTTPFetcher instance
Broken by 070916be4486658d5199d30ae5fb75fb7d857ddd

lib/feed2email/feed.rb
@@ -88,19 +88,21 @@ module Feed2Email
       begin
         handle_redirection!
 
-        HTTPFetcher.new(uri, request_headers: fetch_headers) do |f|
-          if f.response.is_a?(Net::HTTPNotModified)
-            logger.info 'Feed not modified; skipping...'
-            return false
-          end
+        fetcher = HTTPFetcher.new(uri, request_headers: fetch_headers)
 
-          self.last_modified = f.response['last-modified']
-          self.etag = f.response['etag']
-          f.data
+        if fetcher.response.is_a?(Net::HTTPNotModified)
+          logger.info 'Feed not modified; skipping...'
+          return false
         end
+
+        self.last_modified = fetcher.response['last-modified']
+        self.etag = fetcher.response['etag']
+
+        fetcher.data
       rescue => e
         logger.error 'Failed to fetch feed'
         record_exception(e)
+
         false
       end
     end