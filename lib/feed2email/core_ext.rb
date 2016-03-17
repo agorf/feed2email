b86894d6b6a713dd7b0d5469628facf46b6633e1
Angelos Orfanakos <me@agorf.gr>
2016-03-17 23:27:00 +0200

    Replace nil.blank? and String#blank? with manual checks
    Monkey-patching for a one-time use is not worth it.

--- lib/feed2email/core_ext.rb
+++ lib/feed2email/core_ext.rb
@@ -8,17 +8,7 @@ class Numeric
   end
 end
 
-class NilClass
-  def blank?
-    true
-  end
-end
-
 class String
-  def blank?
-    !(self =~ /\A\s*\z/).nil?
-  end
-
   def escape_html
     CGI.escapeHTML(self)
   end
