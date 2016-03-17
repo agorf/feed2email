e58fc6d119cbd216c17b9de8bec050e27a271dd9
Angelos Orfanakos <me@agorf.gr>
2016-03-18 00:55:20 +0200

    Extract type detection into separate methods
    POODR

--- lib/feed2email/feed_analyzer.rb
+++ lib/feed2email/feed_analyzer.rb
@@ -18,27 +18,9 @@ module Feed2Email
     def type
       return unless response
 
-      case fetcher.content_type[/[^;]+/]
-      when 'text/rss', 'text/rss+xml', 'application/rss+xml',
-           'application/rdf+xml', 'application/xml', 'text/xml'
-        return 'rss'
-      when 'text/atom', 'text/atom+xml', 'application/atom+xml'
-        return 'atom'
-      end
-
-      case File.extname(path)
-      when '.rdf', '.rss'
-        return 'rss'
-      when '.atom'
-        return 'atom'
-      end
-
-      case File.basename(path)
-      when 'rss.xml', 'rdf.xml'
-        'rss'
-      when 'atom.xml'
-        'atom'
-      end
+      type_from_content_type(fetcher.content_type[/[^;]+/]) ||
+        type_from_extname(path) ||
+        type_from_basename(path)
     end
 
     private
@@ -53,6 +35,34 @@ module Feed2Email
 
     def path
       fetcher.uri_path
+    end
+
+    def type_from_basename(path)
+      case File.basename(path)
+      when 'rss.xml', 'rdf.xml'
+        'rss'
+      when 'atom.xml'
+        'atom'
+      end
+    end
+
+    def type_from_content_type(content_type)
+      case content_type
+      when 'text/rss', 'text/rss+xml', 'application/rss+xml',
+           'application/rdf+xml', 'application/xml', 'text/xml'
+        'rss'
+      when 'text/atom', 'text/atom+xml', 'application/atom+xml'
+        'atom'
+      end
+    end
+
+    def type_from_extname(path)
+      case File.extname(path)
+      when '.rdf', '.rss'
+        'rss'
+      when '.atom'
+        'atom'
+      end
     end
 
     attr_reader :url
