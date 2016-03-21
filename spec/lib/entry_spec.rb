6907ea6e245fc366810d196b3fe64c71abb43445
Angelos Orfanakos <me@agorf.gr>
2016-03-21 23:18:27 +0200

    Remove redundant Time.now stubbing

--- spec/lib/entry_spec.rb
+++ spec/lib/entry_spec.rb
@@ -255,8 +255,6 @@ describe Feed2Email::Entry do
           let(:now) { Time.parse('2016-03-21 23:09:12 +0200') }
 
           before do
-            allow(Time).to receive(:now).and_return(now)
-
             entry.process
           end
 
