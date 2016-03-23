2a2c7172fa0e93daab3d46d5124890dcb6b15aef
Angelos Orfanakos <me@agorf.gr>
2016-03-23 22:29:12 +0200

    Remove unused capture_stdout and capture_stderr helpers
    RSpec now supports capturing stdout/stderr with the output matcher.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,32 +28,6 @@ RSpec.configure do |config|
   end
 end
 
-def capture_stdout(&block)
-  original_stdout = $stdout
-  $stdout = fake = StringIO.new
-
-  begin
-    yield
-  ensure
-    $stdout = original_stdout
-  end
-
-  fake.string
-end
-
-def capture_stderr(&block)
-  original_stderr = $stderr
-  $stderr = fake = StringIO.new
-
-  begin
-    yield
-  ensure
-    $stderr = original_stderr
-  end
-
-  fake.string
-end
-
 def fixture_path(filename)
   File.join('spec', 'fixtures', filename)
 end
